e2be4d1a73bb0775b0ec021d658a504330c110b2
Sebastian Baez <sdbb.unl@gmail.com>
2020-05-24 18:44:31 +0200

    First commit

--- src/App.js
+++ src/App.js
@@ -3,7 +3,11 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <p>this is working</p>
+    </div>
+  );
 }
 
 export default App;
